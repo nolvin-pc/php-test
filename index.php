14c827ca7a1ca09ff121bd54148819800d6aa4e6
nolvin-pc <vlad_ovsienko2@mail.ru>
2018-09-05 10:20:49 +0300

    File add

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+for($i=0;$i<10;$i++)
+{
+    echo "*";
+}
+echo '<br/>';
+for($i2=0;$i2<10;$i2++)
+{
+    echo "*";
+    for($i=0;$i<16;$i++)
+    {
+        echo "&nbsp;";
+    }
+    echo "*";
+    echo '<br/>';
+}
+for($i=0;$i<10;$i++)
+{
+    echo "*";
+}
